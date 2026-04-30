033d8ce79a4533dcc663820705365abef3721e64
Ivan Burd <p1m37aradox@gmail.com>
2026-04-30 15:42:17 -0700

    docs: sync README to v0.0.3-beta and update core logic

--- bin/setup_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-apt update && apt install -y build-essential python3-dev python3-pip curl libffi-dev libssl-dev
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-export ANDROID_API_LEVEL=34
-export PURE_PYTHON=1
-pip install chromadb mempalace --no-cache-dir --break-system-packages
